fcb1e0c666d33aaeab07a215070d880cdb3ea0bb
ouputsrv <capymob@gmail.com>
2019-02-07 09:32:51 +0000

    Book app V2

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+use Validator;
+
+class BooksController extends Controller
+{
+    // 更新 
+    public function update(Request $request)
+    {
+        // バリデーション
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'item_name' => 'required|min:3|max:255',
+            'item_number' => 'required|min:1|max:3',
+            'item_amount' => 'required|max:6',
+            'published' => 'required',
+        ]);
+        // バリデーション: エラー
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        //データ更新
+        $books = Book::find($request->id);
+        $books->item_name   = $request->item_name;
+        $books->item_number = $request->item_number;
+        $books->item_amount = $request->item_amount;
+        $books->published   = $request->published;
+        $books->save();
+        return redirect('/');
+    }
+
+    public function store(Request $request) 
+    {
+        $validator = Validator::make($request->all(), [
+        'item_name' => 'required|min:3|max:255',
+        'item_number' => 'required | min:1 | max:3',
+        'item_amount' => 'required | max:6',
+         'published'   => 'required',
+        ]);
+    
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        
+        // Eloquent モデル
+        $books = new Book;
+        $books->item_name = $request->item_name;
+        $books->item_number = '1';
+        $books->item_amount = '1000';
+        $books->published = '2017-03-07 00:00:00';
+        $books->save();// 「/」 ルートにリダイレクト
+        return redirect('/');
+    } 
+
+    public function index()
+    {
+        $books = Book::orderBy('created_at', 'asc')->get();
+        return view('books', ['books' => $books]);
+    }
+
+    public function edit()
+    {
+        return view('booksedit', ['book' => $books]);
+    }
+
+    public function delete()
+    {
+        $book->delete();
+        return redirect('/');
+    }
+    
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,74 +14,21 @@
 use App\Book;
 use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    $books = Book::orderBy('created_at', 'asc')->get();
-    return view('books', ['books' => $books]);
-
-});
-
-Route::post('/books', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-            'item_name' => 'required|min:3|max:255',
-            'item_number' => 'required | min:1 | max:3',
-            'item_amount' => 'required | max:6',
-             'published'   => 'required',
-    ]);
-    
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-    
-    // Eloquent モデル
-    $books = new Book;
-    $books->item_name = $request->item_name;
-    $books->item_number = '1';
-    $books->item_amount = '1000';
-    $books->published = '2017-03-07 00:00:00';
-    $books->save();// 「/」 ルートにリダイレクト
-    return redirect('/');
-
-}); 
-
-Route::delete('/book/{book}', function (Book $book) {
-    $book->delete();
-    return redirect('/');
-});
+// 本ダッシュボード表示
+Route::get('/', 'BooksController@index');
 
 
-//更新画面
-Route::post('/booksedit/{books}', function(Book $books) {
-    //{books}id 値を取得 => Book $books id 値の1レコード取得
-    return view('booksedit', ['book' => $books]);
-});
+// 更新処理
+Route::post('/books/update','BooksController@update');
 
+// 登録処理
+Route::post('/books','BooksController@store');
 
-//更新処理
-Route::post('/books/update', function(Request $request){
-    //バリデーション
-        $validator = Validator::make($request->all(), [
-            'id' => 'required',
-            'item_name' => 'required|min:3|max:255',
-            'item_number' => 'required|min:1|max:3',
-            'item_amount' => 'required|max:6',
-            'published' => 'required',
-    ]);
-    //バリデーション:エラー
-        if ($validator->fails()) {
-            return redirect('/')
-                ->withInput()
-                ->withErrors($validator);
-    }
-    //データ更新
-    $books = Book::find($request->id);
-    $books->item_name   = $request->item_name;
-    $books->item_number = $request->item_number;
-    $books->item_amount = $request->item_amount;
-    $books->published   = $request->published;
-    $books->save();
-    return redirect('/');
-});
+// 本ダッシュボード表示
+Route::get('/','BooksController@index');
 
+// 更新画面
+Route::post('/booksedit/{books}','BooksController@edit');
 
+// 本を削除
+Route::delete('/book/{book}','BooksController@destroy');
